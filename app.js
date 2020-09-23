b2a0f37d01940b9944dfbbe8204cd853c0d0a149
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-09-23 19:31:51 -0400

    added functions update state and DOM

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const h1 = document.getElementById('nametag');
+//import funcations and grab DOM elements
 
-h1.addEventListener('name', () => {
-    
-})
+const name = document.getElementById('name-value')
+const h1 = document.getElementById('nametag');
+const button = document.getElementById('submit-button');
+
+// set event listeners to update state and the DOM
+button.addEventListener('click', () => {
+    // GET the input's value
+    const value = name.value;
+
+    // SET the h1's textContent to that value (defined outcome -- validate this it happended)
+    h1.textContent = value;
+
+    // clear the input
+    name.textContent = '';
+});
